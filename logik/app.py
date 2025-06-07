5c8a3505f51fb73da03dc1b68cb6421be8c487a9
Marek Brož <marek.broz@student.sps-cl.cz>
2025-06-07 10:07:15 +0200

    update for vlajkoBiti

--- logik/app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, render_template, request, session, jsonify, redirect, url_for
-from collections import Counter
-import random
-import logging
-
-app = Flask(__name__)
-app.secret_key = 'super_tajny_klic_logik_2025'
